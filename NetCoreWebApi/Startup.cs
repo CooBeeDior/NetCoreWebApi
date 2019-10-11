98fc88daa4291c58a3a33d10c04ac1211ae49f93
tenghao510 <tenghao510@qq.com>
2019-10-11 14:30:05 +0800

    添加对MiniProfiler依赖

--- NetCoreWebApi/Startup.cs
+++ NetCoreWebApi/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -29,6 +30,14 @@ namespace NetCoreWebApi
             services.AddDbContext<MyDbContext>
             (options => options.UseSqlServer(connectionStrings,
                 e => e.MigrationsAssembly("NetCoreWebApi.Model")));
+            // 首先添加一个配置选项，用于访问分析结果：
+            services.AddMiniProfiler(options =>
+            {
+                // 设定弹出窗口的位置
+                options.PopupRenderPosition = RenderPosition.Left;
+                // 设定访问分析结果URL的路由基地址
+                options.RouteBasePath = "/profiler";
+            }).AddEntityFramework();//显示sql
             //注入Swagger服务
             services.AddSwaggerGen(options =>
             {
@@ -41,7 +50,6 @@ namespace NetCoreWebApi
                         Contact = new Contact { Name = "TengHao", Email = "tenghao510@qq.com" },
                         License = new License { Name = "TengHao", Url = "https://blog.csdn.net/weixin_37291339" }
                     });
-
                 // 为 Swagger JSON and UI设置xml文档注释路径
                 var basePath = Path.GetDirectoryName(typeof(Program).Assembly.Location);//获取应用程序所在目录
                 var xmlPath01 = Path.Combine(basePath, "NetCoreWebApi.xml"); 
@@ -49,33 +57,28 @@ namespace NetCoreWebApi
                 var xmlPath = Path.Combine(basePath, "NetCoreWebApi.Model.xml"); 
                 options.IncludeXmlComments(xmlPath);
             });
-            // 首先添加一个配置选项，用于访问分析结果：
-            services.AddMiniProfiler(options =>
-            {
-                // 设定弹出窗口的位置
-                options.PopupRenderPosition = RenderPosition.Left;
-                // 设定在弹出的明细窗口里会显式Time With Children这列
-                options.PopupShowTimeWithChildren = true;
-                // 设定访问分析结果URL的路由基地址
-                options.RouteBasePath = "/profiler";
-            }).AddEntityFramework();//显示sql
+            
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // 响应 HTTP 请求的方式。可将中间件注册到IApplicationBuilder 实例来配置请求管道。
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
+        { 
+            
             //启用Swagger服务
             app.UseSwagger();
+            //把它放在UseMvc()方法之前。 
+            app.UseMiniProfiler();
             app.UseSwaggerUI(c =>
             {
+                c.IndexStream = () => GetType().GetTypeInfo().Assembly.GetManifestResourceStream("NetCoreWebApi.index.html");
+                //访问swagger UI的路由，如http://localhost:<port>/swagger
+                //默认Swagger,我这里不设置
+                c.RoutePrefix = string.Empty;
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
                 c.DefaultModelsExpandDepth(-1); //设置为 - 1 可不显示models
                 c.DocExpansion(DocExpansion.None);//设置为none可折叠所有方法
             });
-            //把它放在UseMvc()方法之前。 
-            app.UseMiniProfiler();
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
